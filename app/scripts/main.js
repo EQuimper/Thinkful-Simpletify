7c93750d4c4e97e1e026e6afe5dd9ec140ad4a57
EQuimper <quimperemanuel@gmail.com>
2016-04-04 13:20:28 -0600

    can read song

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,7 @@ $(function(){
   $('form#getTrack').submit(function(e){
     e.preventDefault();
     var value = $('input#trackName').val();
+    $('.artists ul>li').remove('*');
     getArtistInfo(value)
     $('input#trackName').val('');
   });
@@ -29,53 +30,71 @@ function getArtistInfo(value){
   })
   .done(function(result){
     // console.log(result);
+    result.artists.items = result.artists.items.slice(0, 5);
     $.each(result.artists.items, function(i, item){
       var name = item.name;
       var artistId = item.id;
-      var imageSm = item.images[0].url;
+      var imageSm = item.images.length >= 1 ? item.images[0].url : '';
       showResultArtist(name, imageSm, artistId);
     });
+
+    $('.listArtistItem').on('click', function(e){
+      e.preventDefault();
+
+      var idArtist = $(this).data('artist');
+
+      var request = {
+        country: 'US',
+        limit: 5,
+        Authorization: API
+      };
+
+      $.ajax({
+        url: 'https://api.spotify.com/v1/artists/'+idArtist+'/top-tracks',
+        data: request,
+        datatype: 'json',
+        type: 'GET'
+      })
+      .done(function(result){
+        $('.songs ul>li').remove('*');
+        result.tracks = result.tracks.slice(0, 10);
+        $.each(result.tracks, function(i, item){
+          var preview = item.preview_url;
+          // var trackId = item.id;
+          // console.log(trackId);
+          var trackImg = item.album.images[0].url;
+          var title = item.name;
+          showResultTrack(preview, trackImg, title);
+        });
+        $('.tracksItem').mouseover(function(){
+          $(this).find('.album').attr('src', '../images/play.png').addClass('buttonPlay');
+        }).mouseout(function(){
+          var imgDefault = $(this).find('.album').data('image');
+          $(this).find('.album').attr('src', imgDefault);
+        }).mouseover(function(){
+          var oneTrack = $(this).data('preview')
+          $(this).on('click', function(){
+            $(this).hide();
+            $(this).html('<iframe src="'+oneTrack+'"></iframe>').addClass('player');
+          })
+
+
+        });
+      });
+    });
   });
+
 }
 
 function showResultArtist(name, imageSm, artistId){
   // console.log(name);
   $('.artists ul').append('<li data-artist="'+artistId+'" class="listArtistItem"><img class="img-circle" src="'+imageSm+'" alt="" /><h5 class="name">'+name+'</h5></li>');
 
-  $('.listArtistItem').on('click', function(e){
-    e.preventDefault();
-    var idArtist = $(this).data('artist');
-
-    var request = {
-      country: 'US',
-      limit: 5,
-      Authorization: API
-    };
-
-    $.ajax({
-      url: 'https://api.spotify.com/v1/artists/'+idArtist+'/top-tracks',
-      data: request,
-      datatype: 'json',
-      type: 'GET'
-    })
-    .done(function(result){
-      // console.log(result);
-      $.each(result.tracks, function(i, item){
-        var preview = item.preview_url;
-        var trackImg = item.album.images[0].url;
-        var title = item.name;
-        showResultTrack(preview, trackImg, title);
-      });
-    });
-  });
 }
 
 function showResultTrack(preview, trackImg, title){
-  console.log(trackImg);
   'use strict';
 
-  $('.songs ul').append('<li class="tracksItem"><img class="img-circle" src="'+trackImg+'" alt="" /><h6 class="title">'+title+'</h6></li>');
+  $('.songs ul').append('<li data-preview="'+preview+'" class="tracksItem"><img data-image="'+trackImg+'" class="album img-circle" src="'+trackImg+'" alt="" /><h6 class="title">'+title+'</h6></li>');
 
 }
-
-$('.tracksItem').mouseover().text('<i class="glyphicon glyphicon-play-circle"></i>');
