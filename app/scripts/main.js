818d502ca24005e795f43d33c05b7fe318b2c1e7
EQuimper <quimperemanuel@gmail.com>
2016-04-04 00:59:19 -0600

    Get artist info/image and id and can get id by click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,49 @@
 /* global $:false*/
 
 $(function(){
+  'use strict';
   $('form#getTrack').submit(function(e){
     e.preventDefault();
     var value = $('input#trackName').val();
-    console.log(value);
+    getArtistInfo(value)
     $('input#trackName').val('');
   });
 });
+
+function getArtistInfo(value){
+  'use strict';
+
+  var request = {
+    q: value,
+    type: 'track,artist',
+    limit: 5,
+    Market: 'US',
+    Authorization: API
+  };
+
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search',
+    data: request,
+    datatype: 'json',
+    type: 'GET'
+  })
+  .done(function(result){
+    // console.log(result);
+    $.each(result.artists.items, function(i, item){
+      var name = item.name;
+      var artistId = item.id;
+      var imageSm = item.images[0].url;
+      showResult(name, imageSm, artistId);
+    });
+  });
+}
+
+function showResult(name, imageSm, artistId){
+  // console.log(name);
+  $('.artists ul').append('<li data-artist="'+artistId+'" class="listArtistItem"><img class="img-circle" src="'+imageSm+'" alt="" /><h5 class="name">'+name+'</h5></li>');
+
+  $('.listArtistItem').on('click', function(e, artistId){
+    e.preventDefault();
+    console.log($(this).data('artist'));
+  });
+}
